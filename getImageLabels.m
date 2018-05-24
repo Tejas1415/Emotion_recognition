0f19fd1d94ab7bf958f78d06c52b4ff26eef877b
Tejas Krishna Reddy <37299405+Tejas1415@users.noreply.github.com>
2018-05-24 20:08:38 +0530

    Update getImageLabels.m

--- getImageLabels.m
+++ getImageLabels.m
@@ -1,5 +1,6 @@
 function imageType = getImageLabels(imset)
-% Copyright (c) 2015, MathWorks, Inc.
+% Just labelling the images and the features captured and encoded.
+% categorial features are non numerical data features which cannot be understood by most machine learning algorithms.
     imageType = categorical(repelem({imset.Description}', ...
         [imset.Count], 1));
 end
